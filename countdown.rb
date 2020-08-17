138d6879ae7fea45b2d0ca8f4d90ad97d5889520
Adam Beatty <adam.t.beatt@gmail.com>
2020-08-17 18:59:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(int)
+int.to_i
+while int > 0
+  puts "#{int} SECOND(S)!\n"
+  int -= 1
+end
+
+"HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(int)
+  int.to_i
+  while int > 0
+    puts "#{int} SECOND(S)!\n"
+    int -= 1
+    sleep(1)
+  end
+
+  "HAPPY NEW YEAR!"
+
+
 end
