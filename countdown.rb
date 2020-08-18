817fac937f2be8f53c63fde84d39b1e4da5c802a
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-18 18:52:49 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(num)
+  
+    while num > 0 
+      puts "#{num} seconds!"
+      count-=1
+    end
+    "HAPPY NEW YEAR!"
+  end
 
-def countdown
-end
+  def countdown_with_sleep(num)
+    
+    while num > 0
+      num-=1
+      sleep(1)
+    end
+      puts "HAPPY NEW YEAR!"
+  end
