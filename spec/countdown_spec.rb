d4ae0e5bd630c3b2b060891c39f43e82cad3d656
Julie Garcia <juliegarcia.cali@gmail.com>
2020-08-24 16:10:30 +0000

    Done.

--- spec/countdown_spec.rb
+++ spec/countdown_spec.rb
@@ -1,25 +1,23 @@
-require_relative './spec_helper'
-require 'benchmark'
+def countdown(num)
 
-describe 'LOOPS' do
+ while num > 0
 
-  describe '#countdown' do
-    let(:countdown_output) { "10 SECOND(S)!\n9 SECOND(S)!\n8 SECOND(S)!\n7 SECOND(S)!\n6 SECOND(S)!\n5 SECOND(S)!\n4 SECOND(S)!\n3 SECOND(S)!\n2 SECOND(S)!\n1 SECOND(S)!\n" }
+   puts "#{num} SECOND(S)!"
+  num -= 1
 
-    it "outputs '<n> SECOND(S)!' string to STDOUT for each count" do
-      expect { countdown(10) }.to output(countdown_output).to_stdout
-    end
-
-    it 'returns HAPPY NEW YEAR!' do
-      expect(countdown(12)).to eq "HAPPY NEW YEAR!"
-    end
-  end
-
-  describe '#countdown_with_sleep' do
-    it 'should take at least 5 seconds to execute' do
-      runtime = Benchmark.measure { countdown_with_sleep(5) }
-      expect(runtime.real.to_i >= 5).to be true
-    end
-  end
-
+ end
+  "HAPPY NEW YEAR!"
 end
+
+
+ def countdown_with_sleep(num)
+
+ while num > 0
+
+   puts "#{num} SECOND(S)!"
+  sleep(1)
+  num -= 1
+
+ end
+  "HAPPY NEW YEAR!"
+end 
